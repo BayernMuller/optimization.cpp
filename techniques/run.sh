8c8fa2d2b89c134635f9f618bfd2ff7893c7a5ab
BayernMuller <cjjunk29@gmail.com>
2024-06-10 21:02:02 +0900

    fix: add error handling in shell script

--- techniques/run.sh
+++ techniques/run.sh
@@ -7,6 +7,10 @@ function log() {
     echo -e "\033[1;32m$1\033[0m" >&2
 }
 
+function error() {
+    echo -e "\033[1;31m$1\033[0m" >&2
+}
+
 if [ ! -d "$TARGET" ]; then
     log "Target directory does not exist."
     exit 1
@@ -24,9 +28,17 @@ fi
 if [ -f "$TARGET/Makefile" ]; then
     log "Compiling with Makefile..."
     make -C "$TARGET"
+    if [ $? -ne 0 ]; then
+        error "Compilation failed."
+        exit 1
+    fi
 else
     log "Compiling with g++..."
     g++ -std=c++20 -o "$TARGET/a.out" -O0 "$TARGET/main.cpp" "$OPTION"
+    if [ $? -ne 0 ]; then
+        error "Compilation failed."
+        exit 1
+    fi
 fi
 
 if [ -f "$TARGET/a.out" ]; then
